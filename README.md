e1e9b51d28260aa8a89ddf90520596071b436b49
Thaenkhun <tankoon300950@gmail.com>
2023-07-04 13:38:50 +0700

    first app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 A sample command-line application with an entrypoint in `bin/`, library code
 in `lib/`, and example unit test in `test/`.
+1.ชื้อกาแฟ 4 แก้ว
+2.ชื้อกาแฟ 5 แก้ว แถม 1 แก้ว
+3.ชื้อกาแฟ 10 แก้ว แถม 2 แก้ว
+4.ชื้อกาแฟ 14 แก้ว แถม 3 แก้ว
+5..เกิดขัอผิดพลาด
--- bin/dart_week8.dart
+++ bin/dart_week8.dart
@@ -1,21 +1,35 @@
+import 'dart:html';
 import 'dart:io';
 
 import 'package:test/test.dart';
 
 void main(){
-  print("ชื้อ 5 แถม 1");
-  int coffee;
-  coffee =int.parse(stdin.readLineSync()!);
+  int coffe;
   int price = 60;
-
-  int total = price * coffee;
+  print("เอากี่แก้วครับอ้าย : ");
+  coffe = int.parse(stdin.readLineSync()!);
+  int total = price * coffe;
+  print("ราคารวม = $total บาท");
   print("ค่าใช้จ่าย");
-  
-  if(coffee >= 5){
-    print("จำนวนกาแฟ = ${coffee + 1}แก้ว");
+
+  if ((coffe > 0) & (coffe < 5)){
     print("ราคารวม = $total บาท");
-  }else{
-    print("จำนวนกาแฟ = ${coffee}แก้ว");
+    print("จำนวนกาเเฟ = $coffe เเก้ว");
+  }
+  else if((coffe >= 5) & (coffe < 10)){
     print("ราคารวม = $total บาท");
+    print("จำนวนกาเเฟ = ${coffe + 1} เเก้ว");
+  }
+  else if((coffe >= 10) & (coffe < 14)){
+    print("ราคารวม = $total บาท");
+    print("จำนวนกาเเฟ = ${coffe + 2} เเก้ว");
+  }
+  else if((coffe >= 14)){
+    print("ราคารวม = $total บาท");   
+    print("จำนวนกาเเฟ = ${coffe + 3} เเก้ว");
+  }
+  else{
+    print("ราคารวม = เกิดข้อผิดพลาด");
+    print("จำนวนกาเเฟ = $coffe เเก้ว");
   }
 }
